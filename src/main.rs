7e043e8655573fb2a749990442a02fad2b0ef3ab
Hannes Hallqvist <hannes.hallqvist@hotmail.com>
2024-04-08 15:06:45 +0200

    new structure and better ud1 discovery

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use analyze::Analyzer;
 use clap::Parser;
 
 mod analyze;
@@ -22,10 +23,11 @@ fn main() {
     let file_path = cli.file;
     // let file_path = "/home/hannes/Kth/mex/check-cfi/test/input/cfi_single_indirect";
     let (file_content, offset) = io::read_file(&file_path);
-    let (checked, unchecked) = analyze::disassembled_iced(
-        &file_content,
-        offset,
-        cli.limit_unchecked.unwrap_or_default(),
-    );
+
+    let mut analyzer = Analyzer::new();
+    analyzer.disassemble(&file_content, offset);
+    analyzer.analyze();
+    let (checked, unchecked) = analyzer.get_results();
+
     io::print_results(checked, unchecked, cli.verbose)
 }
