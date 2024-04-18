af5a07c92fef1dab210450d6b026344017ac4b06
Hannes Hallqvist <hannes.hallqvist@hotmail.com>
2024-04-18 13:43:07 +0200

    new approach with safe-listing registers

--- src/analyze.rs
+++ src/analyze.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use iced_x86::{
     Decoder, DecoderOptions, FlowControl, Instruction, InstructionInfoFactory, Mnemonic, OpKind,
@@ -8,7 +8,7 @@ use indicatif::ProgressStyle;
 
 const INSTRUCTION_BUFFER_SIZE: usize = 40;
 const ARGUMENT_LOADING_INSTRUCTION_COUNT: usize = 20;
-const DEBUGGING_IP: u64 = 0;
+const DEBUGGING_IP: u64 = 0x5fc36e;
 
 /*
             idea:
@@ -67,9 +67,9 @@ impl Analyzer {
 
             let flow_control = instruction.flow_control();
 
-            if flow_control == FlowControl::IndirectBranch
-                || flow_control == FlowControl::IndirectCall
-            {
+            if
+            /*flow_control == FlowControl::IndirectBranch*/
+            flow_control == FlowControl::IndirectCall {
                 if !instruction.is_ip_rel_memory_operand() {
                     self.icalls.push(instruction);
                 }
@@ -81,7 +81,8 @@ impl Analyzer {
         progress.finish();
     }
 
-    pub fn analyze(&mut self) {
+    #[allow(unused)]
+    pub fn analyze_bckwd(&mut self) {
         let progress = indicatif::ProgressBar::new(self.icalls.len() as u64);
         progress.set_style(
             ProgressStyle::with_template("Analyzing:     [{bar} {percent}%]")
@@ -99,10 +100,296 @@ impl Analyzer {
         progress.finish();
     }
 
+    // new strat
+    // find jump to ud1
+    // mark registers involved in us ending up in ud1 as safe
+    // keep track of safe registers
+    // when call, check if called register are safe
+    pub fn analyze_fwd(&mut self) {
+        let progress = indicatif::ProgressBar::new(self.icalls.len() as u64);
+        progress.set_style(
+            ProgressStyle::with_template("Analyzing:     [{bar} {percent}%]")
+                .unwrap()
+                .progress_chars("=>-"),
+        );
+
+        for icall in self.icalls.iter() {
+            match self.is_cfi_checked_2(icall) {
+                Ok(_) => self.checked.push(icall.clone()),
+                Err(msg) => self.unchecked.push((icall.clone(), msg)),
+            }
+            progress.inc(1);
+        }
+        progress.finish();
+    }
+
     pub fn get_results(&self) -> (&Vec<Instruction>, &Vec<(Instruction, String)>) {
         (&self.checked, &self.unchecked)
     }
 
+    fn is_cfi_checked_2(&self, icall: &Instruction) -> Result<(), String> {
+        let Some(icall_index) = self.address_map.get(&icall.ip()) else {
+            return Err("Instruction not found in vector".to_string());
+        };
+
+        let mut predecessor_index = icall_index - 1;
+
+        let mut trusted = HashSet::<Register>::new();
+
+        let call_target = get_register_or_mem_base(icall, 0);
+        let mut ud1_found = false;
+        let mut ud1_index = 0;
+
+        // iterate the instructions, backwards
+        // we want to indentify the ud1 or jump to ud1, and from that determine what registers are
+        // trusted. Any register involved in a `cmp jmp ud1` sequence is considered trusted
+        while predecessor_index > 0 {
+            if icall.ip() == DEBUGGING_IP {
+                eprintln!("Checking predecessor at index {}", predecessor_index);
+            }
+
+            let Some(instruction) = self.instructions.get(predecessor_index) else {
+                return Err("Backward search failed".to_string());
+            };
+
+            // if we have determined our call target as trusted, stop looking
+            if trusted.contains(&call_target) {
+                if icall.ip() == DEBUGGING_IP {
+                    eprintln!("Target trusted");
+                }
+                // we cannot return yet, because we have to make sure our trusted register are not
+                // touched before the call
+                break;
+            }
+
+            match instruction.mnemonic() {
+                // if we find the top of a function, stop looking
+                Mnemonic::Push => {
+                    if instruction.op0_register() == Register::RBP {
+                        if icall.ip() == DEBUGGING_IP {
+                            eprintln!("Breaking on function border.");
+                        }
+                        break;
+                    }
+                }
+                // if we find a load into a safe register, stop looking.
+                // this should be the lea of the jump- or vtable
+                // Mnemonic::Lea => {
+                //     if instruction.op0_kind() == OpKind::Register
+                //         && trusted.contains(&instruction.op0_register())
+                //     {
+                //         if icall.ip() == DEBUGGING_IP {
+                //             eprintln!("Breaking on lea to trusted");
+                //         }
+                //         break;
+                //     }
+                // }
+                Mnemonic::Jae | Mnemonic::Ja | Mnemonic::Jne => {
+                    if !ud1_found {
+                        // look up the branch target in the address map
+                        let Some(jump_target_index) =
+                            self.address_map.get(&instruction.near_branch_target())
+                        else {
+                            predecessor_index -= 1;
+                            continue;
+                        };
+
+                        // get the branch target instruction
+                        let Some(branch_target) = self.instructions.get(*jump_target_index) else {
+                            predecessor_index -= 1;
+                            continue;
+                        };
+
+                        // check if the branch target is a ud1
+                        if branch_target.mnemonic() == Mnemonic::Ud1 {
+                            if icall.ip() == DEBUGGING_IP {
+                                eprintln!("Found jump to ud1");
+                            }
+                            ud1_found = true;
+                            ud1_index = predecessor_index;
+
+                            // on occation, there might be instructions placed between the jcc and
+                            // cmp. These are ignored as long as they dont affect the control flow
+                            let mut cmp: &Instruction;
+                            loop {
+                                predecessor_index -= 1;
+                                let opt_cmp = self.instructions.get(predecessor_index);
+
+                                match opt_cmp {
+                                    Some(instr) => cmp = instr,
+                                    None => {
+                                        return Err(format!(
+                                            "Failed to get instruction at index {}",
+                                            predecessor_index
+                                        ))
+                                    }
+                                }
+
+                                // compare found
+                                if cmp.mnemonic() == Mnemonic::Cmp {
+                                    break;
+                                }
+
+                                // skip if not affecting control flow
+                                if cmp.flow_control() != FlowControl::Next {
+                                    return Err(
+                                        "Something breaking control flow between jcc and cmp"
+                                            .to_string(),
+                                    );
+                                }
+                            }
+
+                            // mark the compared registers as trusted
+                            trusted.insert(cmp.op0_register());
+                            if icall.ip() == DEBUGGING_IP {
+                                eprintln!("Trusting {:?}", cmp.op0_register());
+                            }
+                            match cmp.op1_kind() {
+                                OpKind::Register => {
+                                    if icall.ip() == DEBUGGING_IP {
+                                        eprintln!("Trusting {:?}", cmp.op1_register());
+                                    }
+                                    trusted.insert(cmp.op1_register());
+                                }
+                                _ => (),
+                            }
+                        }
+                    }
+                }
+                Mnemonic::Ud1 => {
+                    if !ud1_found {
+                        if icall.ip() == DEBUGGING_IP {
+                            eprintln!("Found stray ud1");
+                        }
+                        // ud1 found. Now we may start marking registers as trusted
+                        // before the ud1, there should be a conditional jump
+                        ud1_index = predecessor_index;
+                        predecessor_index -= 1;
+                        let Some(jump) = self.instructions.get(predecessor_index) else {
+                            return Err("Could not get jump predeceding the ud1".to_string());
+                        };
+
+                        if !jump.is_jcc_short_or_near() {
+                            // TODO: resove route to call
+                            return Err("long jump".to_string());
+                        }
+                        // before the conditional jump, there should be a compare
+                        predecessor_index -= 1;
+                        let Some(cmp) = self.instructions.get(predecessor_index) else {
+                            return Err("Could not get cmp predeceding the ud1".to_string());
+                        };
+
+                        if cmp.mnemonic() != Mnemonic::Cmp {
+                            return Err(
+                                "Something other than a cmp preceeded the jcc, ud1".to_string()
+                            );
+                        }
+
+                        // mark the compared registers as trusted
+                        trusted.insert(cmp.op0_register());
+                        if icall.ip() == DEBUGGING_IP {
+                            eprintln!("Trusting {:?}", cmp.op0_register());
+                        }
+                        match cmp.op1_kind() {
+                            OpKind::Register => {
+                                if icall.ip() == DEBUGGING_IP {
+                                    eprintln!("Trusting {:?}", cmp.op1_register());
+                                }
+                                trusted.insert(cmp.op1_register());
+                            }
+                            _ => (),
+                        }
+
+                        // we can now propagate the trusted registers
+                        ud1_found = true;
+                    }
+                }
+
+                Mnemonic::Mov | Mnemonic::Sub => {
+                    // if something is moved into a trusted register, that value is also trusted
+                    if ud1_found
+                        && trusted.contains(&instruction.op0_register())
+                        && instruction.op1_kind() == OpKind::Register
+                    {
+                        trusted.insert(instruction.op1_register());
+                        if icall.ip() == DEBUGGING_IP {
+                            eprintln!("Trusting {:?}", instruction.op1_register());
+                            eprintln!("Trusted is now {:?}", trusted);
+                        }
+                    }
+                }
+
+                // TODO: ud1 jumps, calls (?) skip on way up? Or err?
+                _ => (),
+            }
+            predecessor_index -= 1;
+        }
+
+        let iterated_instructions = icall_index - predecessor_index;
+
+        // at the point of the ud1 compare, we now have a set of trusted registers.
+        // if these are now overwritten, we do not consider them trusted anymore
+        // we start iterating from the place where we found the ud1
+
+        // TODO: CFG this
+        for instruction in self.instructions.iter().skip(ud1_index) {
+            if icall.ip() == DEBUGGING_IP {
+                eprintln!("forward roll, instruction: 0x{:x}", instruction.ip());
+            }
+            // when we reach our icall, the search is complete
+            if instruction.ip() == icall.ip() {
+                break;
+            }
+
+            //  let info = self.info_factory.info(instruction);
+            // let read_registers = info.used_registers();
+
+            match instruction.mnemonic() {
+                Mnemonic::Mov => {
+                    // if we move from a untrusted to a trusted register, that register is no longer
+                    // trusted
+                    if instruction.op1_kind() == OpKind::Register
+                        || instruction.op1_kind() == OpKind::Memory
+                    {
+                        let to = get_register_or_mem_base(instruction, 0);
+                        let from = get_register_or_mem_base(instruction, 1);
+
+                        // untrusted -> trusted, remove trusted
+                        if trusted.contains(&to) && !trusted.contains(&from) {
+                            trusted.remove(&to);
+                            if icall.ip() == DEBUGGING_IP {
+                                eprintln!("fwd: Untrusting {:?}", to);
+                                eprintln!("Trusted is now {:?}", trusted);
+                                eprintln!("instruction: 0x{:x}", instruction.ip());
+                            }
+                        }
+                        // trusted -> untrusted, add trusted
+                        if trusted.contains(&from) && !trusted.contains(&to) {
+                            trusted.insert(to);
+                            if icall.ip() == DEBUGGING_IP {
+                                eprintln!("fwd: Trusting {:?}", from);
+                                eprintln!("Trusted is now {:?}", trusted);
+                                eprintln!("instruction: 0x{:x}", instruction.ip());
+                            }
+                        }
+                    }
+                }
+                // TODO: if we call a function, we now only trust callee-saved
+                _ => (),
+            }
+        }
+
+        if trusted.contains(&call_target) {
+            Ok(())
+        } else {
+            Err(format!(
+                "Call target not trusted. Looked at {} instructions. Trusted registers: {:?}",
+                iterated_instructions, trusted
+            ))
+        }
+    }
+
+    #[allow(dead_code)]
     fn is_cfi_checked(&self, icall: &Instruction) -> Result<(), &str> {
         // look up the instructions index in the vector
         let Some(instruction_index) = self.address_map.get(&icall.ip()) else {
@@ -330,4 +617,12 @@ fn is_mem_op_matching(ins_a: &Instruction, ins_b: &Instruction) -> bool {
         && ins_a.memory_base() == ins_b.memory_base()
         && ins_a.memory_index() == ins_b.memory_index()
         && ins_a.memory_index_scale() == ins_b.memory_index_scale();
+}
+
+fn get_register_or_mem_base(instruction: &Instruction, position: u32) -> Register {
+    match instruction.op_kind(position) {
+        OpKind::Register => instruction.op_register(position),
+        OpKind::Memory => instruction.memory_base(),
+        unknown => unimplemented!("unknown opkind {:?}", unknown),
+    }
 }
