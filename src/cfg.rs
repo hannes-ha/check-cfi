caf550a3e618670ee5db019e04126c99c3ad7d42
Hannes Hallqvist <hannes.hallqvist@hotmail.com>
2024-04-23 00:05:33 +0200

    improved backtracking algorithm

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 use iced_x86::Mnemonic;
 use petgraph::{graphmap::DiGraphMap, visit::Dfs};
 
@@ -21,7 +23,8 @@ impl Cfg {
     }
 
     pub fn add_node(&mut self, value: u64) -> u64 {
-        self.fwd_graph.add_node(value)
+        self.fwd_graph.add_node(value);
+        self.bwd_graph.add_node(value)
     }
 
     pub fn contains_node(&mut self, value: u64) -> bool {
@@ -30,10 +33,10 @@ impl Cfg {
 
     pub fn add_edge(&mut self, from: u64, to: u64) {
         self.fwd_graph.add_edge(from, to, ());
+        self.bwd_graph.add_edge(to, from, ());
     }
 
-    // TODO: flip logic for bwd_graph
-    // shold use backward graph
+    // looks backwards for the ud1 and related compare
     pub fn find_compare(&self, analyzer: &Analyzer, entrypoint: u64) -> Result<u64, String> {
         let mut dfs = Dfs::new(&self.bwd_graph, entrypoint);
 
@@ -41,38 +44,39 @@ impl Cfg {
         let mut cmp_ip = 0;
         let mut ud1_found = false;
 
+        let mut itt = 0;
         while let Some(ip) = dfs.next(&self.bwd_graph) {
+            itt += 1;
             let instruction = analyzer.get_instruction(ip)?;
 
-            if instruction.mnemonic() == Mnemonic::Cmp {
-                cmp_ip = instruction.ip();
+            if instruction.mnemonic() == Mnemonic::Ud1 {
+                if ud1_found {
+                    panic!("duplicate ud1");
+                }
+                ud1_found = true;
                 continue;
             }
 
-            if instruction.mnemonic() == Mnemonic::Ud1 && cmp_ip > 0 {
-                ud1_found = true;
-                break;
-            }
-
-            if instruction.is_jcc_short_or_near() && cmp_ip > 0 {
+            if instruction.is_jcc_short_or_near() || instruction.is_jmp_short_or_near() {
                 let jcc_target = analyzer.get_instruction(instruction.near_branch_target())?;
                 if jcc_target.mnemonic() == Mnemonic::Ud1 {
-                    // compare and ud1 branch found. We are done
+                    if ud1_found {
+                        panic!("duplicate ud1");
+                    }
                     ud1_found = true;
-                    break;
+                    continue;
                 }
             }
 
-            // TODO: slack for mov between cmp and jcc
-            cmp_ip = 0;
+            if instruction.mnemonic() == Mnemonic::Cmp && ud1_found {
+                cmp_ip = instruction.ip();
+                break;
+            }
         }
+
         if cmp_ip > 0 && ud1_found {
             return Ok(cmp_ip);
         }
-        Err("Could not find compare".to_string())
+        Err(format!("could not find compare after {} iterations", itt))
     }
-
-    pub fn assert_call_target_trusted() {}
-
-    pub fn assert_call_register_untouched() {}
 }
